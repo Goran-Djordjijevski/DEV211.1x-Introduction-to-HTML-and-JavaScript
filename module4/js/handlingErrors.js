9bf8741bc99f13e760bf47d5b9ea6b621bf3bd70
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-10-18 20:57:08 +0200

    Handling errors.

---
+++ module4/js/handlingErrors.js
@@ -0,0 +1,13 @@
+function correctMethodName() {
+    return "Output Message";
+}
+
+
+
+try {
+    var result = csctMethodName(3);
+    
+    document.writeln(result);
+} catch (error) {
+    document.writeln("An error occurred");
+}
